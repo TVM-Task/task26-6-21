5c465f2a1993539d2258432f3be722c9920df8a3
BhavaniAruna-2000 <83978708+BhavaniAruna-2000@users.noreply.github.com>
2021-06-26 22:05:20 +0530

    attribute
    attribute in angular

---
+++ attributedirective.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-attributedirective',
+  templateUrl: './attributedirective.component.html',
+  styleUrls: ['./attributedirective.component.css']
+})
+export class AttributedirectiveComponent implements OnInit {
+  flag:boolean=true;
+
+  style={
+    color:'orange'
+  
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
